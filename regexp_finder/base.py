547ebef2511abd53ed1155d835e8185f6964d756
Александр Умнов <alexmac@Aleksandrs-MacBook-Air.local>
2022-03-28 20:17:13 +0300

    tests, default cls and env

--- regexp_finder/base.py
+++ regexp_finder/base.py
@@ -1,24 +1,27 @@
-from abc import ABC, abcstractmethod, abstractmethod
+from abc import ABC, abstractmethod
 from typing import List, Dict, Optional
 from common.dataset import Dataset
 import re
 
 class RegexpFinderBase(ABC):
-    def __init__(self, default_dict: Optional[Dict[str, str]] = {}) -> None:
+    def __init__(
+        self, 
+        default_dict: Optional[Dict[str, str]] = {}, 
+        default_cls: Optional[str] = None
+    ) -> None:
         self._regexp_dict: Dict[str, str] = default_dict
+        self._default_cls = default_cls
     
     def get_regexp_dict(self) -> Dict[str, str]:
         return self._regexp_dict
 
-    @abcstractmethod
-    def fit(self, dataset: Dataset) -> None:
-        return NotImplementedError
-
     def predict(self, texts: List[str]) -> List[str]:
         labels = []
         for text in texts:
-            for cls, regexp in self._regexp_dict:
-                if re.match(regexp, text):
+            for cls, regexp in self._regexp_dict.items():
+                if re.search(regexp, text):
                     labels.append(cls)
-                    continue
+                    break
+            else:
+                labels.append(self._default_cls)
         return labels
--- tests/regexp_finder/test_unit.py
+++ tests/regexp_finder/test_unit.py
@@ -2,10 +2,11 @@ from regexp_finder.base import RegexpFinderBase
 
 def test_regexp_match():
     finder = RegexpFinderBase(
-        {
+        default_dict={
             "pos": "positive",
             "neg": "negative"
-        }
+        },
+        default_cls='neg'
     )
 
     texts = [
@@ -13,8 +14,9 @@ def test_regexp_match():
         "negative text",
         "another positive text",
         "and another positive text",
-        "and another negative text"
+        "and another negative text",
+        "a text without matches"
     ]
 
     predicted_labels = finder.predict(texts)
-    assert predicted_labels == ['pos', 'neg', 'pos', 'pos', 'neg']
+    assert predicted_labels == ['pos', 'neg', 'pos', 'pos', 'neg', 'neg']
